6a3281563023adc7ed44325c8e485d059f7db5c0
Ce Gao <cegao@tensorchord.ai>
2025-02-01 08:21:59 +0800

    feat: OpenAI batch API part 1
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

---
+++ src/vllm_router/files/storage.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from vllm_router.files.files import OpenAIFile
+
+
+class Storage(ABC):
+    DEFAULT_USER_ID = "uid_default"
+    DEFAULT_PURPOSE = "batch"
+
+    @abstractmethod
+    async def save_file(
+        self,
+        file_id: str = None,
+        user_id: str = DEFAULT_USER_ID,
+        file_name: str = None,
+        content: bytes = None,
+        purpose: str = DEFAULT_PURPOSE,
+    ) -> OpenAIFile:
+        pass
+
+    @abstractmethod
+    async def save_file_chunk(
+        self,
+        file_id: str,
+        user_id: str = DEFAULT_USER_ID,
+        chunk: bytes = None,
+        purpose: str = DEFAULT_PURPOSE,
+        offset: int = 0,
+    ) -> None:
+        pass
+
+    async def get_file(
+        self, file_id: str, user_id: str = DEFAULT_USER_ID
+    ) -> OpenAIFile:
+        pass
+
+    @abstractmethod
+    async def get_file_content(
+        self, file_id: str, user_id: str = DEFAULT_USER_ID
+    ) -> bytes:
+        pass
+
+    @abstractmethod
+    async def list_files(self, user_id: str = DEFAULT_USER_ID) -> List[str]:
+        pass
+
+    @abstractmethod
+    async def delete_file(self, file_id: str, user_id: str = DEFAULT_USER_ID):
+        pass
+
+
+def initialize_storage(storage_type: str, base_path: str = None) -> Storage:
+    if storage_type == "local_file":
+        from vllm_router.files.file_storage import FileStorage
+
+        return FileStorage(base_path)
+    else:
+        raise ValueError(f"Unsupported storage type: {storage_type}")
