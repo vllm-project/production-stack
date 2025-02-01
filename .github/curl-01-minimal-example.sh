8f62940b6da1892d65bd66437bb765de7c26f015
Shaoting Feng <shaotingf@uchicago.edu>
2025-02-01 04:52:23 +0000

    artifact different names
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- .github/curl-01-minimal-example.sh
+++ .github/curl-01-minimal-example.sh
@@ -3,5 +3,5 @@
 # Curl and save output
 [ ! -d "output-01-minimal-example" ] && mkdir output-01-minimal-example
 chmod -R 777 output-01-minimal-example
-result_model=$(curl -s http://$1:$2/models | tee output-01-minimal-example/models.json)
-result_query=$(curl -X POST http://$1:$2/completions -H "Content-Type: application/json" -d '{"model": "facebook/opt-125m", "prompt": "Once upon a time,", "max_tokens": 10}' | tee output-01-minimal-example/query.json)
+result_model=$(curl -s http://$1:$2/models | tee output-01-minimal-example/models-01-minimal-example.json)
+result_query=$(curl -X POST http://$1:$2/completions -H "Content-Type: application/json" -d '{"model": "facebook/opt-125m", "prompt": "Once upon a time,", "max_tokens": 10}' | tee output-01-minimal-example/query-01-minimal-example.json)
--- .github/curl-04-multiple-models.sh
+++ .github/curl-04-multiple-models.sh
@@ -3,8 +3,8 @@
 # Curl and save output
 [ ! -d "output-04-multiple-models" ] && mkdir output-04-multiple-models
 chmod -R 777 output-04-multiple-models
-result_model=$(curl -s http://$1:$2/models | tee output-04-multiple-models/models.json)
+result_model=$(curl -s http://$1:$2/models | tee output-04-multiple-models/models-04-multiple-models.json)
 
 source /usr/local/bin/conda-init
 conda activate llmstack
-result_query=$(python3 tutorials/assets/example-04-openai.py --openai_api_base "http://$1:$2/" | tee output-04-multiple-models/query.json)
+result_query=$(python3 tutorials/assets/example-04-openai.py --openai_api_base "http://$1:$2/" | tee output-04-multiple-models/query-04-multiple-models.json)
