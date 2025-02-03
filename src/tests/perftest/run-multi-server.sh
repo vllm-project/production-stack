0c9b48c4ce98ecc0e12e4e57720220c335e7051b
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-02-03 17:59:43 +0000

    Make `shellcheck` happy again
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- src/tests/perftest/run-multi-server.sh
+++ src/tests/perftest/run-multi-server.sh
@@ -6,5 +6,5 @@ if [[ $# -ne 2 ]]; then
 fi
 
 for i in $(seq 1 "$1"); do
-    python3 ./fake-openai-server.py --port "900$i" --speed $2 &
+    python3 ./fake-openai-server.py --port "900$i" --speed "$2" &
 done
