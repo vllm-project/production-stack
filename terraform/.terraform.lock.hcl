eac2ca49712644428fc7f9f1a7a04861b52f6f5e
falconlee236 <falconlee236@cau.ac.kr>
2025-02-24 16:21:44 +0900

    init terraform

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "6.21.0"
+  constraints = "~> 6.0"
+  hashes = [
+    "h1:pZhpGdzOtzGkX38PIYbXWilwA/LtWXQ22dkt6Fh2DAQ=",
+    "zh:1c2462367d92f6f8f6c527115905f7cca78e48cf5d5bc7448d3beeb7c9e895eb",
+    "zh:3644dbd09c3740e6d843e035de34a74ed41ffc32e7ed04a19aecddc4c57334cc",
+    "zh:3a586bbb9a9c6463c975a94ddd4671f2a84992a2c169bfb2f5053c2cea55849c",
+    "zh:4ae96672e6a52a077760a11c95946ec9d3f84f7ab84c0ba3c0cb66c3d3580d59",
+    "zh:9c26b3dbc1f9a594d1d07b6a25ce089f8463e8331324f4ecde73829e9d1d5ee5",
+    "zh:b99a602111d6ca5842c852ac1eff5c009f1d75492e355ea25f3dbd6e008e4d9a",
+    "zh:d45100c41c940c35c07fae2876f6cc654328d405077f01d268e8bd5a25b56c30",
+    "zh:de6e14e85a9ea2322a4fd971fde3b71071e7b6435a12dbcd3b8c5f42765e8b3c",
+    "zh:e22f6b54cfebb0c1a0991d83adc83b3d454ba6d9b5c21574af135799b488ed66",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f6964268874a15788dfed47a26cb880718f47c94cba7c8a0284b70921fec807b",
+    "zh:ff51b3e83149798ce6c7545688fe3b9703b04d5c0376cd55215a93767144f40e",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/helm" {
+  version     = "2.17.0"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:K5FEjxvDnxb1JF1kG1xr8J3pNGxoaR3Z0IBG9Csm/Is=",
+    "zh:06fb4e9932f0afc1904d2279e6e99353c2ddac0d765305ce90519af410706bd4",
+    "zh:104eccfc781fc868da3c7fec4385ad14ed183eb985c96331a1a937ac79c2d1a7",
+    "zh:129345c82359837bb3f0070ce4891ec232697052f7d5ccf61d43d818912cf5f3",
+    "zh:3956187ec239f4045975b35e8c30741f701aa494c386aaa04ebabffe7749f81c",
+    "zh:66a9686d92a6b3ec43de3ca3fde60ef3d89fb76259ed3313ca4eb9bb8c13b7dd",
+    "zh:88644260090aa621e7e8083585c468c8dd5e09a3c01a432fb05da5c4623af940",
+    "zh:a248f650d174a883b32c5b94f9e725f4057e623b00f171936dcdcc840fad0b3e",
+    "zh:aa498c1f1ab93be5c8fbf6d48af51dc6ef0f10b2ea88d67bcb9f02d1d80d3930",
+    "zh:bf01e0f2ec2468c53596e027d376532a2d30feb72b0b5b810334d043109ae32f",
+    "zh:c46fa84cc8388e5ca87eb575a534ebcf68819c5a5724142998b487cb11246654",
+    "zh:d0c0f15ffc115c0965cbfe5c81f18c2e114113e7a1e6829f6bfd879ce5744fbb",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
---
+++ terraform/cluster.tf
@@ -0,0 +1,141 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster#gpu_driver_installation_config-2
+resource "google_container_cluster" "primary" {
+  name = var.cluster_name
+  location = var.zone
+
+  min_master_version = "1.31.5-gke.1169000" # --cluster-version "1.31.5-gke.1023000"
+
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  release_channel {
+    channel = "REGULAR" # --release-channel "regular"
+  }
+
+  logging_config { # --logging=SYSTEM,WORKLOAD
+    enable_components = [ "SYSTEM_COMPONENTS", "WORKLOADS" ]
+  }
+
+  monitoring_config { # --monitoring=SYSTEM,STORAGE,POD,DEPLOYMENT,STATEFULSET,DAEMONSET,HPA,CADVISOR,KUBELET 
+    enable_components = [ 
+      "SYSTEM_COMPONENTS",
+      "STORAGE",
+      "POD",
+      "DEPLOYMENT",
+      "STATEFULSET",
+      "DAEMONSET",
+      "HPA",
+      "CADVISOR",
+      "KUBELET"
+    ]
+    managed_prometheus { # --enable-managed-prometheus
+      enabled = true
+    }
+  }
+
+  networking_mode = "VPC_NATIVE" # --enable-ip-alias
+  network = "default" # --network "projects/$GCP_PROJECT/global/networks/default"
+  subnetwork = "default" # --subnetwork "projects/$GCP_PROJECT/regions/us-central1/subnetworks/default"
+  ip_allocation_policy {} # need to vpc-native cluster
+  enable_intranode_visibility = false # --no-enable-intra-node-visibility
+
+  
+  private_cluster_config {
+    enable_private_nodes = false # --no-enable-master-authorized-networks
+    enable_private_endpoint = false # --no-enable-google-cloud-access
+  }
+
+  addons_config { # --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+    gce_persistent_disk_csi_driver_config {
+      enabled = false
+    }
+  }
+
+  binary_authorization { # --binauthz-evaluation-mode=DISABLED
+    evaluation_mode = "DISABLED"
+  }
+
+  node_config { # --enable-shielded-nodes
+    shielded_instance_config {
+      enable_secure_boot = true 
+    }
+
+    # guest_accelerator { # -- gpu nodes
+    #   type  = "nvidia-tesla-t4"
+    #   count = 1
+    #   gpu_driver_installation_config {
+    #     gpu_driver_version = "DEFAULT"
+    #   }
+    # }
+
+    # machine_type = "n1-standard-8" # default = "e2-medium"
+  }
+
+  maintenance_policy {
+    recurring_window {
+      start_time = "2024-01-01T00:00:00Z"
+      end_time   = "2024-01-02T00:00:00Z"
+      recurrence = "FREQ=WEEKLY;BYDAY=SA,SU"
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name = "${var.cluster_name}-node-pool"
+  location = var.zone # --node-locations "$ZONE"
+  cluster = google_container_cluster.primary.name
+  node_count = 1 # --num-nodes "1"
+
+  node_config {
+    machine_type = "n2d-standard-8" # --machine-type "n2d-standard-8"
+    image_type = "COS_CONTAINERD" # --image-type "COS_CONTAINERD"
+    disk_type = "pd-balanced" # --disk-type "pd-balanced"
+    disk_size_gb = 100 # --disk-size "100"
+    
+
+    metadata = {
+      disable-legacy-endpoints = "true"  # # --metadata disable-legacy-endpoints=true
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+
+  management {
+    auto_repair = true # --enable-autoupgrade
+    auto_upgrade = true # --enable-autorepair
+  }
+
+  upgrade_settings {
+    max_surge = 1 # --max-surge-upgrade 1
+    max_unavailable = 0 # --max-unavailable-upgrade 0
+  }
+
+}
+
+# Helm Release 설정
+resource "helm_release" "vllm" {
+  name       = "vllm"
+  repository = "https://vllm-project.github.io/production-stack"
+  chart      = "vllm-stack"
+
+  values = [
+    file(var.setup_yaml)
+  ]
+
+  depends_on = [
+    google_container_cluster.primary,
+    google_container_node_pool.primary_nodes
+  ]
+}
