d82788f10e8ed31d696275ebb19b72a88e260d37
YuhanLiu11 <yliu738@wisc.edu>
2025-02-27 02:15:59 +0000

    Updating benchmark scripts
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

---
+++ benchmarks/multi-round-qa/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <model> <base url>"
+    exit 1
+fi
+
+MODEL=$1
+BASE_URL=$2
+
+# CONFIGURATION
+NUM_USERS=15
+NUM_ROUNDS=20
+
+SYSTEM_PROMPT=1000 # Shared system prompt length
+CHAT_HISTORY=20000 # User specific chat history length
+ANSWER_LEN=100 # Generation length per round
+
+
+run_benchmark() {
+    python3 ./multi-round-qa.py \
+        --num-users $NUM_USERS \
+        --num-rounds $NUM_ROUNDS \
+        --qps $1 \
+        --shared-system-prompt $SYSTEM_PROMPT \
+        --user-history-prompt $CHAT_HISTORY \
+        --answer-len $ANSWER_LEN \
+        --model "$MODEL" \
+        --base-url "$BASE_URL" \
+        --output $2 \
+        --log-interval 30 \
+        --time 100
+}
+
+key=$3
+for qps in 0.3 0.5 0.7 0.9 1.1; do
+run_benchmark $qps $key_output_$qps.csv
+done
---
+++ benchmarks/multi-round-qa/warmup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <model> <base url>"
+    exit 1
+fi
+
+MODEL=$1
+BASE_URL=$2
+QPS=$3
+
+# CONFIGURATION
+NUM_USERS=20
+NUM_ROUNDS=5
+
+SYSTEM_PROMPT=1000 # Shared system prompt length
+CHAT_HISTORY=20000 # User specific chat history length
+ANSWER_LEN=100 # Generation length per round
+
+OUTPUT_FILE=$4
+
+warmup() {
+    # Warm up the vLLM with a lot of user queries
+    python3 ./multi-round-qa.py \
+        --num-users 1 \
+        --num-rounds 2 \
+        --qps 2 \
+        --shared-system-prompt $SYSTEM_PROMPT \
+        --user-history-prompt $CHAT_HISTORY \
+        --answer-len $ANSWER_LEN \
+        --model "$MODEL" \
+        --base-url "$BASE_URL" \
+        --output /tmp/warmup.csv \
+        --log-interval 30 \
+        --time $(($NUM_USERS / 2 + 2))
+}
+
+warmup
