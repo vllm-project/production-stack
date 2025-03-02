05137cb4a22dc85277c9bfc3c718784da2760b1d
Huamin Chen <hchen@redhat.com>
2025-03-02 13:50:57 -0500

    downgrade huggingface-hub to avoid breaking changes
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "python-multipart==0.0.20",
         "sentence-transformers==2.2.2",
         "faiss-cpu==1.10.0",
+        "huggingface-hub==0.25.2",  # downgrade to 0.25.2 to avoid breaking changes
     ],
     entry_points={
         "console_scripts": [
