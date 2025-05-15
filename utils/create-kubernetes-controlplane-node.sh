0aeea8f977f8548393159bcb4076074dbeccb7b0
insukim1994 <insu.kim@moreh.io>
2025-05-15 17:11:51 +0000

    [Doc] Added cri-o container runtime installation script & a script to create a control plane node.
    Signed-off-by: insukim1994 <insu.kim@moreh.io>

---
+++ utils/create-kubernetes-controlplane-node.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo swapoff -a
+sudo modprobe br_netfilter
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Refer to https://v1-32.docs.kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
+# for more information.
+# This script will create a Kubernetes cluster using kubeadm.
+
+# On one of your nodes which to become a control node, execute following command:
+sudo kubeadm init --cri-socket=unix:///var/run/crio/crio.sock
+
+# The output will look like this:
+# --------------------------------------------------------------------------------
+# Your Kubernetes control-plane has initialized successfully!
+
+# To start using your cluster, you need to run the following as a regular user:
+
+#   mkdir -p $HOME/.kube
+#   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#   sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Alternatively, if you are the root user, you can run:
+
+#   export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# You should now deploy a pod network to the cluster.
+# Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
+#   https://kubernetes.io/docs/concepts/cluster-administration/addons/
+
+# Then you can join any number of worker nodes by running the following on each as root:
+
+# kubeadm join <YOUR_CONTROL_PLANE_NODE_IP> --token <YOUR_GENERATED_TOKEN> \
+#         --discovery-token-ca-cert-hash <YOUR_GENERATED_CA_CERT_HASH>
+# --------------------------------------------------------------------------------
---
+++ utils/install-cri-o.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Refer to https://github.com/cri-o/packaging/blob/main/README.md#distributions-using-deb-packages
+# for more information.
+
+# Install the dependencies for adding repositories
+sudo apt-get update
+sudo apt-get install -y software-properties-common curl
+
+export CRIO_VERSION=v1.32
+
+# Add the CRI-O repository
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+    sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+    sudo tee /etc/apt/sources.list.d/cri-o.list
+
+# Install the packages
+sudo apt-get update
+sudo apt-get install -y cri-o
+
+# Start CRI-O
+sudo systemctl start crio.service
