4fd13ce675862e036cf4e01ad336075c64bf2eee
Shaoting Feng <shaotingf@uchicago.edu>
2025-07-22 23:42:04 +0000

    Add cache: pip
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- src/vllm_router/app.py
+++ src/vllm_router/app.py
@@ -94,7 +94,6 @@ async def lifespan(app: FastAPI):
     engine_stats_scraper.close()
 
     logger.info("Closing service discovery module")
-    logger.info("sss")
     service_discovery = get_service_discovery()
     service_discovery.close()
 
