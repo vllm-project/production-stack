c4296f5dbc31d99e3134f52bd25d75b5c57241d3
Jon OILLARBURU <jon.oillarburu@multiversecomputing.com>
2025-08-26 16:21:01 +0200

    remove useless log
    Signed-off-by: jonoillar <jonirvingrubio@gmail.com>

--- src/vllm_router/service_discovery.py
+++ src/vllm_router/service_discovery.py
@@ -701,8 +701,6 @@ class K8sPodIPServiceDiscovery(ServiceDiscovery):
         is_container_ready = self._check_pod_ready(
             pod.status.container_statuses
         )
-
-        logger.info(f"{is_container_ready=}")
         # Pod is ready if container is ready and pod is not terminating
         is_pod_ready = is_container_ready and not is_pod_terminating
 
